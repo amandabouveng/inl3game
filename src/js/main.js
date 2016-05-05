5033788b505acfc1ad231c75ad0b00c7873a8afc
Amanda Bouveng <amandabouveng@gmail.com>
2016-05-05 16:45:18 +0200

    started to create static planet

--- src/js/main.js
+++ src/js/main.js
@@ -5,18 +5,17 @@ var moon = new Image();
 var uranus = new Image();
 
 function init(){
-  sun.src = 'img/sun.svg';
-  mars.src = 'img/mars.svg';
-  mars.id = 'planet_mars';
-  console.log(mars);
-  moon.src = 'img/moon.svg';
-  uranus.src = 'img/uranus.svg';
-  window.requestAnimationFrame(draw);
+  	console.log(mars);
+  	document.getElementById('space').addEventListener('click', draw);
+  	window.requestAnimationFrame(draw);
 }
 
 function draw() {
- 	var ctx = document.getElementById('space').getContext('2d');
-
+	sun.src = 'img/sun.svg';
+  	mars.src = 'img/mars.svg';
+  	mars.id = 'mars';
+  	
+	var ctx = document.getElementById('space').getContext('2d');
   	ctx.globalCompositeOperation = 'destination-over';
   	ctx.clearRect(0,0,1000,1000); // clear canvas
 
@@ -26,10 +25,11 @@ function draw() {
   	ctx.translate(500,300);
 
     //Mars
- 	var time = new Date();
-  	ctx.rotate( ((2*Math.PI)/6)*time.getSeconds() + ((2*Math.PI)/6000)*time.getMilliseconds() );
+    var time = new Date();
+    ctx.rotate( ((2*Math.PI)/6)*time.getSeconds() + ((2*Math.PI)/6000)*time.getMilliseconds() );
   	ctx.translate(100,0);
   	ctx.drawImage(mars,-25,0, 50, 50);
+
 
   	ctx.restore();
   	ctx.beginPath();
